4e10cae5dc4a737feaaace942dc7f386d58c9e97
sizeof <625327545@qq.com>
2019-08-09 12:25:56 +0800

    爬取GitHub的Pull request并存储为CSV文件

--- src/test/java/com/github/hcsp/io/CrawlerTest.java
+++ src/test/java/com/github/hcsp/io/CrawlerTest.java
@@ -1,11 +1,13 @@
 package com.github.hcsp.io;
 
 import com.opencsv.CSVReader;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.kohsuke.github.GHPullRequest;
@@ -19,7 +21,7 @@ public class CrawlerTest {
 
         CSVReader reader = new CSVReader(new BufferedReader(new FileReader(tmp)));
         List<String[]> lines = reader.readAll();
-        Assertions.assertArrayEquals(lines.get(0), new String[] {"number", "author", "title"});
+        Assertions.assertArrayEquals(lines.get(0), new String[]{"number", "author", "title"});
         Assertions.assertTrue(lines.size() > 30);
 
         String[] lastLine = lines.get(30);
