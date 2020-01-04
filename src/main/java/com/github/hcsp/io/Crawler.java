8d11f158a50001e2285329706b8020f44b5ca8cb
monalisali <monalisali@hotmail.com>
2020-01-04 16:08:19 +0800

    s

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -1,6 +1,14 @@
 package com.github.hcsp.io;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import sun.net.www.http.HttpClient;
+
 import java.io.File;
+import java.io.IOException;
 
 public class Crawler {
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，读取前n个Pull request并保存至csvFile指定的文件中，格式如下：
@@ -8,4 +16,13 @@ public class Crawler {
     // 12345,blindpirate,这是一个标题
     // 12345,FrankFang,这是第二个标题
     public static void savePullRequestsToCSV(String repo, int n, File csvFile) {}
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("aa");
+        CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+    }
 }
+
+
+
