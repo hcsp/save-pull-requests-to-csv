2ea96577fa741bb5049d78dbc15acecde58ed25d
Bowen Wu <bwwu@iflytek.com>
2021-07-01 17:09:13 +0800

    feat: use org.lohsuke.github-api library

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -1,18 +1,17 @@
 package com.github.hcsp.io;
 
-import com.alibaba.fastjson.JSON;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Crawler {
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，读取前n个Pull request并保存至csvFile指定的文件中，格式如下：
@@ -20,78 +19,34 @@ public class Crawler {
     // 12345,blindpirate,这是一个标题
     // 12345,FrankFang,这是第二个标题
     public static void savePullRequestsToCSV(String repo, int n, File csvFile) throws IOException {
-        String owner = repo.split("/")[0];
-        String name = repo.split("/")[1];
-        String url = "https://api.github.com";
-        HttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(url + "/repos/" + owner + "/" + name + "/pulls?per_page=" + n);
-        HttpResponse httpResponse = httpClient.execute(httpGet);
-        System.out.println(httpResponse);
-        HttpEntity entity = httpResponse.getEntity();
-        InputStream inputStream = entity.getContent();
-        StringBuilder stringBuilder = new StringBuilder();
-        while (true) {
-            int b = inputStream.read();
-            if (b == -1) {
+        GitHub github = GitHub.connectAnonymously();
+        GHRepository repository = github.getRepository(repo);
+        List<GHPullRequest> pullRequests = repository.getPullRequests(GHIssueState.OPEN);
+
+        List<String> list = new java.util.ArrayList<>(Collections.singletonList("number,author,title"));
+
+        for (GHPullRequest pullRequest : pullRequests) {
+            list.add(getLine(pullRequest));
+            if (list.size() >= n + 1) {
                 break;
             }
-            stringBuilder.append((char) b);
         }
 
-        List<Pull> pullList = JSON.parseArray(String.valueOf(stringBuilder), Pull.class);
-        EntityUtils.consume(entity);
         FileWriter fileWriter = new FileWriter(csvFile);
-        fileWriter.write("number,author,title");
-        for (Pull pull : pullList) {
-            fileWriter.write(System.lineSeparator());
-            fileWriter.write(pull.getNumber() + "," + pull.getUser().getLogin() + "," + pull.getTitle());
-        }
+        fileWriter.write(list.stream().collect(Collectors.joining(System.lineSeparator())));
         fileWriter.close();
+
     }
 
-    private static class Pull {
-        private int number;
-        private User user;
-        private String title;
-
-        public int getNumber() {
-            return number;
-        }
-
-        public void setNumber(int number) {
-            this.number = number;
-        }
-
-        public User getUser() {
-            return user;
-        }
-
-        public void setUser(User user) {
-            this.user = user;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-    }
-
-    private static class User {
-        private String login;
-
-        public String getLogin() {
-            return login;
-        }
-
-        public void setLogin(String login) {
-            this.login = login;
+    public static String getLine(GHPullRequest pullRequest) {
+        try {
+            return pullRequest.getNumber() + "," + pullRequest.getUser().getLogin() + "," + pullRequest.getTitle();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        savePullRequestsToCSV("gradle/gradle", 3, new File("xxx.txt"));
+        savePullRequestsToCSV("gradle/gradle", 3, new File("pr.csv"));
     }
 }
